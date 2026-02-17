6405f0bd3e77fdbfdd6bb2ac9d5167733c628a56
Viacheslav Luchko <luchko@ittest-team.ru>
2026-02-17 09:27:55 +0300

    Add tests, add checking tests on ci

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	apiKey := "test-api-key"
+	normalHeader := http.Header{}
+
+	normalHeader.Add("Authorization", "ApiKey "+apiKey)
+
+	key, err := GetAPIKey(normalHeader)
+
+	if err != nil {
+		t.Errorf("Error getting normal key")
+		return
+	}
+
+	//Break intentionally
+	if key != apiKey || true {
+		t.Errorf("Keys do not match")
+	}
+}
+
+func TestGetAPIKeyWithError(t *testing.T) {
+	badHeader := http.Header{}
+
+	_, err := GetAPIKey(badHeader)
+
+	if err == nil {
+		t.Errorf("No error received on invalid header")
+	}
+}
